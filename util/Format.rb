cd6102b2f52e9f02c7f9c85a80e29f351a1690fb
Pavlov <oleg.pav.m@gmail.com>
2017-02-20 07:00:49 +0300

    Доработки утилиты формата.

--- util/Format.rb
+++ util/Format.rb
@@ -1,12 +1,19 @@
 class Format
-	singleton
 
-	def readable_num(num)
+	def self.readable_num(num)
 		'%1.8f' % num.to_f
 	end
 
-	def time_back(timestamp)
+	def self.time_back(timestamp)
 		(Time.now - timestamp).to_i
+	end
+
+	def self.date_to_i(date)
+		if date.is_a? Integer
+			date
+		else
+			date.to_time.to_i
+		end
 	end
 
 end
