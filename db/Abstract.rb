f3e73df99793360982a74a8ce64e773da379dc3b
Pavlov <oleg.pav.m@gmail.com>
2017-02-20 08:32:53 +0300

    Модель базы.

--- db/Abstract.rb
+++ db/Abstract.rb
@@ -1,11 +1,52 @@
 class Abstract
 
-	def initialize(connection, pair)
+	def initialize(connection:, profile: nil, stock: nil, pair: nil)
 		@connection = connection
-		@pair = pair
+
+		@profile_name = profile
+		@stock_name = stock
+		@pair_name = pair
+
+		@stock_context = [@stock_name]
+		@profile_context = [@stock_name, @profile_name]
+		@pair_context = [@stock_name, @profile_name, @pair_name]
 	end
 
 	protected
+
+	def make_params(values)
+		data = []
+
+		values.each do |key, value|
+			data.push("#{key}='#{value}'") if value
+		end
+
+		if data.length == 0
+			nil
+		else
+			data.join(', ')
+		end
+	end
+
+	def get_single(query, params = [])
+		result = nil
+
+		exec(query, params).each do |row|
+			result = row
+		end
+
+		result
+	end
+
+	def get_many(query, params = [])
+		result = []
+
+		exec(query, params).values.each do |row|
+			result.push(row[0])
+		end
+
+		result
+	end
 
 	def exec(query, params = [])
 		if params.length > 0
