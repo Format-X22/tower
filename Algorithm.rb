18c6080e144c0c0a42227450ba5dc74f7b68e448
Pavlov <oleg.pav.m@gmail.com>
2017-02-21 22:47:51 +0300

    Торговый алгоритм.

---
+++ Algorithm.rb
@@ -0,0 +1,52 @@
+require_relative 'AbstractDSL'
+
+class Algorithm < AbstractDSL
+
+	def trade
+		if delisted?
+			stop_trade!
+			return
+		end
+
+		if stop? or wait?
+			return
+		end
+
+		if btc_pump? or new_coin_add?
+			sell!
+			return
+		end
+
+		unless open_order?
+			next_state
+		end
+
+		trade_by_state
+	end
+
+	def next_state
+		case state
+			when 'buy'  then state 'hold'
+			when 'hold' then state 'calm'
+			when 'calm'
+				if red_candle?
+					state 'buy'
+				end
+			else
+		end
+	end
+
+	def trade_by_state
+		if open_order?
+			replace
+			return
+		end
+
+		case state
+			when 'buy'  then buy
+			when 'hold' then sell
+			else
+		end
+	end
+
+end
