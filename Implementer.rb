c1a5c6866b228a8619b1df3955e0f0f222334540
Pavlov <oleg.pav.m@gmail.com>
2017-02-23 22:18:34 +0300

    Сильная переписка имплементора.

--- Implementer.rb
+++ Implementer.rb
@@ -4,80 +4,48 @@ require_relative 'Executor'
 class Implementer < Executor
 
 	def state(state = nil)
-		if state
-			meta.state = state
-		else
-			meta.state
+		unless state
+			return meta.state
 		end
+
+		case state
+			when 'hold'
+				meta.sell_start_time = now
+			when 'calm'
+				meta.calm = now + meta.calm_offset
+			else
+		end
+
+		meta.extra_btc = 0
+		meta.state = state
 	end
 
 	def buy
-		#
+		rate = optimal_ask_rate
+		unused_btc = earned_capital(meta.sell_start_time) + meta.extra_btc
+		amount = unused_btc / rate
+
+		buy_order(rate, amount)
 	end
 
 	def sell
-		#
-	end
-
-	def sell!
-		#
+		sell_order(sell_rate, money)
 	end
 
 	def replace
 		case order.type
-			when 'buy'
-				rate = optimal_ask_rate
-				amount = order.rate * order.amount / rate
-
-				replace_order(order.id, rate, amount)
-			when 'sell'
-				rate = meta.low * profile.top_price * btc_sigma
-				amount = order.amount + money
-				min = optimal_bid_rate * profile.min_sell_mul
-
-				if rate < min
-					rate = min
-				end
-
-				replace_order(order.id, rate, amount)
+			when 'buy'  then replace_buy
+			when 'sell' then replace_sell
 			else
 		end
 	end
 
-	def stop_trade!
-		#
-	end
-
 	def red_candle?
-		#
+		tick.open > tick.close
 	end
 
 	def open_order?
-		#
-	end
-
-	def delisted?
-		delisted = false
-
-		stock_news.each do |post|
-			if delisting_words?(post.text) and pair_name?(post.text)
-				delisted = true
-			end
-		end
-
-		delisted
-	end
-
-	def new_coin_add?
-		new_coin = false
-
-		stock_news.each do |post|
-			if add_coin_words?(post.text) and now - profile.new_coin_wait_offset < post.date
-				new_coin = true
-			end
-		end
-
-		new_coin
+		order.open.length > 0
 	end
 
 	def stop?
@@ -86,6 +54,49 @@ class Implementer < Executor
 
 	def wait?
 		meta.calm > now
+	end
+
+	private
+
+	def replace_buy
+		rate = optimal_ask_rate
+		amount = order.rate * order.amount / rate
+
+		replace_order(order.id, rate, amount)
+	end
+
+	def replace_sell
+		rate = sell_rate
+		amount = order.amount + money
+
+		replace_order(order.id, rate, amount)
+	end
+
+	def sell_rate
+		rate = meta.low * profile.top_price * btc_sigma
+		min = optimal_bid_rate * profile.min_sell_mul
+
+		if rate < min
+			rate = min
+		end
+
+		rate
+	end
+
+	def optimal_ask_rate
+		#
+	end
+
+	def optimal_bid_rate
+		#
+	end
+
+	def earned_capital(from)
+		#
+	end
+
+	def btc_sigma
+		#
 	end
 
 end
