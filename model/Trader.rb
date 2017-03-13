56bed952647a2ca96cb4aa7d38a06077bbfcfc55
Oleg <oleg.pav.m@gmail.com>
2017-03-13 20:22:10 +0300

    Модель трейдера.

--- model/Trader.rb
+++ model/Trader.rb
@@ -3,23 +3,23 @@ require_relative 'Abstract'
 class Trader < Abstract
 
 	def stop_trade
-		#
+		@db.stop_trade
 	end
 
 	def buy(rate, amount)
-		#
+		@stock.buy(rate, amount)
 
-		log_trade('BUY', rate * amount)
+		@db.log_trade('BUY', rate * amount)
 	end
 
 	def sell(rate, amount)
-		#
+		@stock.sell(rate, amount)
 
-		log_trade('SELL', rate * amount)
+		@db.log_trade('SELL', rate * amount)
 	end
 
 	def replace(id, rate, amount)
-		#
+		@stock.replace(id, rate, amount)
 	end
 
 end
