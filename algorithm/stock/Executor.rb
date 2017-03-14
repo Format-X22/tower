87650f3440ce33d1ef1fa2218c67214459f75f91
Oleg <oleg.pav.m@gmail.com>
2017-03-14 13:40:34 +0300

    Скелет экзекутора бирж.

--- algorithm/stock/Executor.rb
+++ algorithm/stock/Executor.rb
@@ -1,3 +1,25 @@
-class Executor
-	#
+require_relative '../Abstract'
+
+class Executor < Abstract
+
+	def initialize(context)
+		#
+	end
+
+	def profile
+		#
+	end
+
+	def pairs
+		#
+	end
+
+	def sell_order(pair, btc = nil)
+		#
+	end
+
+	def stock_news
+		#
+	end
+
 end
