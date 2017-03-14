b4394f9cfb2e82ce02650cbcdf8ac70f1042e35c
Oleg <oleg.pav.m@gmail.com>
2017-03-14 17:42:12 +0300

    Экзекутор биржи.

--- algorithm/stock/Executor.rb
+++ algorithm/stock/Executor.rb
@@ -1,25 +1,40 @@
-require_relative '../Abstract'
+require_relative '_'
 
-class Executor < Abstract
+class Algorithm::Stock::Executor < Algorithm::Abstract
 
 	def initialize(context)
-		#
+		super()
+		@context = context
+		@profile = Model::Profile.new(context)
+		@pairs   = Model::Pairs.new(context)
+		@trader  = Model::Trader.new(context)
+		@twitter = Model::Twitter.new(context)
+		@glass   = Model::Glass.new(context)
+		@money   = Model::Money.new(context)
 	end
 
 	def profile
-		#
+		@profile.get
 	end
 
 	def pairs
-		#
+		@pairs
 	end
 
 	def sell_order(pair, btc = nil)
-		#
+		@context.pair = pair
+
+		rate = @glass.get.top_bid_rate
+
+		if btc
+			@trader.sell(rate, btc / rate)
+		else
+			@trader.sell(rate, @money.get)
+		end
 	end
 
 	def stock_news
-		#
+		@twitter.news
 	end
 
 end
