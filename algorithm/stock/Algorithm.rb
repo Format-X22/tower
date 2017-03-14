ec6b3f1ef34fa52fa4482e2ddd6c1134fb3b5334
Oleg <oleg.pav.m@gmail.com>
2017-03-14 10:58:14 +0300

    Начало переработки алгоритма биржи.

--- algorithm/stock/Algorithm.rb
+++ algorithm/stock/Algorithm.rb
@@ -8,20 +8,19 @@ class Algorithm < Implementer
 		end
 
 		if delisted_in_bag?
-			mark_trade_off(delisted)
-			sell(delisted)
+			stop_delisted
+			sell_delisted
 			return false
 		end
 
 		if listed_recently?
-			sell(all_traded_pairs)
+			sell_all_traded
 			wait_listed_hype_end
 			return false
 		end
 
 		if harvesting?
-			ratably_decrement_pairs_on(harvesting_usd)
-			ratably_sell_part(all_traded_pairs, harvesting_usd)
+			ratably_sell_harvesting_part
 			stop_harvesting
 			return false
 		end
---
+++ algorithm/stock/Stater.rb
@@ -0,0 +1,49 @@
+require_relative 'Executor'
+
+class Stater < Executor
+
+	def stop?
+		#
+	end
+
+	def wait?
+		#
+	end
+
+	def delisted_in_bag?
+		#
+	end
+
+	def stop_delisted
+		#
+	end
+
+	def sell_delisted
+		#
+	end
+
+	def listed_recently?
+		#
+	end
+
+	def sell_all_traded
+		#
+	end
+
+	def wait_listed_hype_end
+		#
+	end
+
+	def harvesting?
+		#
+	end
+
+	def ratably_sell_harvesting_part
+		#
+	end
+
+	def stop_harvesting
+		#
+	end
+
+end
