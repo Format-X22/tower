468bd371984f50fc1a44175fcd740f10b54eb058
Oleg <oleg.pav.m@gmail.com>
2017-03-15 16:05:09 +0300

    Экзекутор глобального алгоритма, но ещё с недописанным имплементером.

--- algorithm/global/Executor.rb
+++ algorithm/global/Executor.rb
@@ -1,13 +1,34 @@
 require_relative '_'
 
 class Algorithm::Global::Executor < Algorithm::Abstract
+	attr_reader :context
 
-	def initialize
-		super
+	def initialize(db_name, stocks_keys)
+		super()
+
+		connection = DataBase::Connection.new(db_name)
+		database   = DataBase::Driver.new(connection)
+
+		@context = Model::Context.new(database)
+		@config  = Model::Config.new(@context, stocks_keys)
+		@logger  = Model::Logger.new(@context)
+		@pairs   = Model::Pairs.new(@context)
 	end
 
 	def config
 		@config.get
+	end
+
+	def pairs
+		@pairs
+	end
+
+	def log(message)
+		@logger.log(message)
+	end
+
+	def log_error(error)
+		@logger.log_error(error)
 	end
 
 end
--- model/Config.rb
+++ model/Config.rb
@@ -2,6 +2,12 @@ require_relative '_'
 
 class Model::Config < Model::Abstract
 
+	def initialize(context, stock_keys)
+		super(context)
+
+		#
+	end
+
 	def get
 		struct = Util::HashStruct.new(@db.config)
 
