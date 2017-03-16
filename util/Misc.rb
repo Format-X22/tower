9519be6b37e0c3d39468803465c9210d0d085ec8
Oleg <oleg.pav.m@gmail.com>
2017-03-16 20:59:10 +0300

    Доработка утилит.

--- util/Misc.rb
+++ util/Misc.rb
@@ -18,16 +18,20 @@ module Util_Misc
 		'%1.8f' % num.to_f
 	end
 
-	def timestamp_back(timestamp)
-		(Time.now - timestamp).to_i
-	end
-
 	def date_to_i(date)
 		if date.is_a? Integer
-			date
-		else
-			date.to_time.to_i
+			return date
 		end
+
+		if date.is_a? BigDecimal
+			return date.to_i
+		end
+
+		if date.is_a? Date or date.is_a? DateTime
+			return date.to_time.to_i
+		end
+
+		raise 'Unknown date type'
 	end
 
 end
