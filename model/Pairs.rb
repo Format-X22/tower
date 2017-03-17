e91e6f50991c162d8384be9948ee245efd3dcd0c
Oleg <oleg.pav.m@gmail.com>
2017-03-17 18:42:03 +0300

    Модель пар - получение данных.

--- model/Pairs.rb
+++ model/Pairs.rb
@@ -3,19 +3,19 @@ require_relative '_'
 class Model_Pairs < Model_Abstract
 
 	def all
-		#
+		parse @db.pairs
 	end
 
 	def traded
-		#
+		parse @db.pairs(traded: true)
 	end
 
 	def listed
-		#
+		parse @db.pairs(listed: true)
 	end
 
 	def delisted
-		#
+		parse @db.pairs(delisted: true)
 	end
 
 	def usdt
@@ -36,6 +36,17 @@ class Model_Pairs < Model_Abstract
 
 	def decrement_btc(pair, btc)
 		#
+	end
+
+	private
+
+	def parse(data)
+		raw = Util_HashStruct.new(data)
+
+		raw.traded   = raw.traded
+		raw.listed   = raw.listed
+		raw.delisted = raw.delisted
+		raw.add_time = parse_date(raw.add_time)
 	end
 
 end
